c5917b9d2bcce7ccb2d27aab77078778b79a354b
NoLogsVpn <romeo.mihalcea@gmail.com>
2020-11-07 22:59:26 +0200

    feat: macos killswitch

---
+++ macos-killswitch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# For documentation about running this script please visit our repo at:
+# 	https://github.com/nologs-vpn/killswitch
+
+function log {
+	echo "[Killswitch][$(date)] - $1"
+}
+
